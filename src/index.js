445eb3545f18b82b2d827adda545fd5897f1a3e3
Jesse Tomchak <jtomchak@gmail.com>
2017-07-11 21:20:35 -0700

    The great journey of react starts here!!!!

--- src/index.js
+++ src/index.js
@@ -1,15 +1,2 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-var MyFirstComponent = function() {
-  return React.createElement(
-    "div",
-    null,
-    React.createElement("h1", null, "This is my first component!")
-  );
-};
-
-ReactDOM.render(
-  React.createElement(MyFirstComponent),
-  document.getElementById("app")
-);
