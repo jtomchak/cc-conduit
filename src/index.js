24a55ef4bbb3aba3cd8ce190e2edda565c405c13
Jesse Tomchak <jtomchak@gmail.com>
2017-07-11 22:34:09 -0700

    that's a lot of markup

--- src/index.js
+++ src/index.js
@@ -16,15 +16,36 @@ var MyTitle = function(props) {
 };
 //TODO: Create MyDescription component using props.color, props.size and props.description
 // use that component after each MyTitle to add some descriptive details
+var MyDescription = function(props) {
+  return (
+    <div>
+      <p style={{ fontSize: props.size, color: props.color }}>
+        {props.details}
+      </p>
+    </div>
+  );
+};
 
 //composit component
 var SecondSuperSweet = function() {
   return (
     <div>
       <MyTitle title="Monster Town" color="burlywood" />
+      <MyDescription details="It's a nice place" color="burlywood" size="18" />
       <MyTitle title="Pizza Bagels" color="greenyellow" />
+      <MyDescription
+        details="Melt your face off"
+        color="greenyellow"
+        size="18"
+      />
       <MyTitle title="Docker Pants" color="peru" />
+      <MyDescription details="Looking Good" color="peru" size="18" />
       <MyTitle title=".NET Yeah ok" color="yellowgreen" />
+      <MyDescription
+        details="I mean, if you have to"
+        color="yellowgreen"
+        size="18"
+      />
     </div>
   );
 };
