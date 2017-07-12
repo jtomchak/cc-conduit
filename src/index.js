87c0ba40711d1c80f9a5b1e2bb983f82e4e8e148
Jesse Tomchak <jtomchak@gmail.com>
2017-07-11 21:45:52 -0700

    sweet components

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+let ce = React.createElement;
+
+var myTitle = function() {
+  return ce("div", null, ce("h1", null, "This is pretty freakin' sweet"));
+};
+
+var secondSuperSweet = function() {
+  return ce(
+    "div",
+    null,
+    ce(myTitle, null),
+    ce(myTitle, null),
+    ce(myTitle, null)
+  );
+};
+
+ReactDOM.render(ce(secondSuperSweet), document.getElementById("app"));
