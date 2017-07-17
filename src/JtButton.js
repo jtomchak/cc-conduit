562424715f438d2719552268f3c81fcaeacc46e8
Jesse Tomchak <jtomchak@gmail.com>
2017-07-16 20:33:01 -0700

    updated with solution

---
+++ src/JtButton.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const JtButton = props => {
+  return (
+    <button style={buttonStyle} onClick={props.onClick}>
+      {props.title}
+    </button>
+  );
+};
+
+const buttonStyle = {
+  backgroundColor: "#4CAF50" /* Green */,
+  borderWidth: 0,
+  color: "white",
+  paddingTop: 16,
+  paddingBottom: 16,
+  paddingRight: 32,
+  paddingLeft: 32,
+  fontSize: 16
+};
+
+export default JtButton;
--- src/index.js
+++ src/index.js
@@ -1,33 +1,51 @@
 import React from "react";
 import { render } from "react-dom";
-
-//the parameters passed in would be props
-const Counter = ({ count, onIncrement, onDecrement, onReset }) => {
-  return (
-    <div>
-      <h1>
-        {count}
-      </h1>
-      <button onClick={onIncrement}>+</button>
-      <button onClick={onReset}>Reset</button>
-      <button onClick={onDecrement}>-</button>
-    </div>
-  );
-};
+import JtButton from "./JtButton";
 
 class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentCount: 0
+    };
+  }
+
+  onIncrement = () => {
+    this.setState((prevState, props) => {
+      return { currentCount: prevState.currentCount + 1 };
+    });
+  };
+
+  onDecrement = () => {
+    this.setState((prevState, props) => {
+      return { currentCount: prevState.currentCount - 1 };
+    });
+  };
+
+  onReset = () => {
+    this.setState((prevState, props) => {
+      return { currentCount: 0 };
+    });
+  };
+
   render() {
     return (
       <div>
-        <h1>
-          {this.props.count}
+        <h1 style={counterStyle}>
+          {this.state.currentCount}
         </h1>
-        <button onClick={this.props.onIncrement}>+</button>
-        <button onClick={this.props.onReset}>Reset</button>
-        <button onClick={this.props.onDecrement}>-</button>
+        <div style={{ textAlign: "center" }}>
+          <JtButton title="+" onClick={this.onIncrement} />
+          <JtButton title="Reset" onClick={this.onReset} />
+          <JtButton title="-" onClick={this.onDecrement} />
+        </div>
       </div>
     );
   }
 }
+const counterStyle = {
+  fontSize: 32,
+  textAlign: "center"
+};
 
 render(<Counter />, document.getElementById("app"));
